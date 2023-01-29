c764b42a75b059eafad03d70bbb9e7a7706aeee9
wangchunyang2 <wangchunyang2@yy.com>
2023-01-29 20:38:47 +0800

    feat: favicon 更新

--- index.js
+++ index.js
@@ -1,60 +1,84 @@
-const favicons = require('favicons')
-const path = require('path')
+import fs from 'fs/promises'
+import { favicons } from "favicons"
+import makeDir from 'make-dir'
+import toIco from 'to-ico'
 
-// Source image(s). `string`, `buffer` or array of `string`
-const source = path.resolve(__dirname, './favicons/desktop 2018-10-30 19-20-31.mp4.png')
+const source = "./favicons/logo.png"; // Source image(s). `string`, `buffer` or array of `string`
 
 const configuration = {
-    path: '.',                                // Path for overriding default icons path. `string`
-    appName: null,                            // Your application's name. `string`
-    appShortName: null,                       // Your application's short_name. `string`. Optional. If not set, appName will be used
-    appDescription: null,                     // Your application's description. `string`
-    developerName: null,                      // Your (or your developer's) name. `string`
-    developerURL: null,                       // Your (or your developer's) URL. `string`
-    dir: 'auto',                              // Primary text direction for name, short_name, and description
-    lang: 'en-US',                            // Primary language for name and short_name
-    background: '#fff',                       // Background colour for flattened icons. `string`
-    theme_color: '#fff',                      // Theme color user for example in Android's task switcher. `string`
-    appleStatusBarStyle: 'black-translucent', // Style for Apple status bar: "black-translucent", "default", "black". `string`
-    display: 'standalone',                    // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
-    orientation: 'any',                       // Default orientation: "any", "natural", "portrait" or "landscape". `string`
-    scope: '/',                               // set of URLs that the browser considers within your app
-    start_url: '/?homescreen=1',              // Start URL when launching the application from a device. `string`
-    version: '1.0',                           // Your application's version string. `string`
-    logging: false,                           // Print logs to console? `boolean`
-    pixel_art: false,                         // Keeps pixels "sharp" when scaling up, for pixel art.  Only supported in offline mode.
-    loadManifestWithCredentials: false,       // Browsers don't send cookies when fetching a manifest, enable this to fix that. `boolean`
-    icons: {
-      // Platform Options:
-      // - offset - offset in percentage
-      // - background:
-      //   * false - use default
-      //   * true - force use default, e.g. set background for Android icons
-      //   * color - set background for the specified icons
-      //   * mask - apply mask in order to create circle icon (applied by default for firefox). `boolean`
-      //   * overlayGlow - apply glow effect after mask has been applied (applied by default for firefox). `boolean`
-      //   * overlayShadow - apply drop shadow after mask has been applied .`boolean`
-      //
-      android: true,              // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      appleIcon: true,            // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      appleStartup: true,         // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      coast: true,                // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      favicons: true,             // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      firefox: true,              // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      windows: true,              // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-      yandex: true                // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
-    }
+  path: "/", // Path for overriding default icons path. `string`
+  appName: null, // Your application's name. `string`
+  appShortName: null, // Your application's short_name. `string`. Optional. If not set, appName will be used
+  appDescription: null, // Your application's description. `string`
+  developerName: null, // Your (or your developer's) name. `string`
+  developerURL: null, // Your (or your developer's) URL. `string`
+  dir: "auto", // Primary text direction for name, short_name, and description
+  lang: "en-US", // Primary language for name and short_name
+  background: "#fff", // Background colour for flattened icons. `string`
+  theme_color: "#fff", // Theme color user for example in Android's task switcher. `string`
+  appleStatusBarStyle: "black-translucent", // Style for Apple status bar: "black-translucent", "default", "black". `string`
+  display: "standalone", // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
+  orientation: "any", // Default orientation: "any", "natural", "portrait" or "landscape". `string`
+  scope: "/", // set of URLs that the browser considers within your app
+  start_url: "/?homescreen=1", // Start URL when launching the application from a device. `string`
+  preferRelatedApplications: false, // Should the browser prompt the user to install the native companion app. `boolean`
+  relatedApplications: undefined, // Information about the native companion apps. This will only be used if `preferRelatedApplications` is `true`. `Array<{ id: string, url: string, platform: string }>`
+  version: "1.0", // Your application's version string. `string`
+  pixel_art: false, // Keeps pixels "sharp" when scaling up, for pixel art.  Only supported in offline mode.
+  loadManifestWithCredentials: false, // Browsers don't send cookies when fetching a manifest, enable this to fix that. `boolean`
+  manifestMaskable: false, // Maskable source image(s) for manifest.json. "true" to use default source. More information at https://web.dev/maskable-icon/. `boolean`, `string`, `buffer` or array of `string`
+  icons: {
+    // Platform Options:
+    // - offset - offset in percentage
+    // - background:
+    //   * false - use default
+    //   * true - force use default, e.g. set background for Android icons
+    //   * color - set background for the specified icons
+    //
+    android: true, // Create Android homescreen icon. `boolean` or `{ offset, background }` or an array of sources
+    appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background }` or an array of sources
+    appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background }` or an array of sources
+    favicons: true, // Create regular favicons. `boolean` or `{ offset, background }` or an array of sources
+    windows: true, // Create Windows 8 tile icons. `boolean` or `{ offset, background }` or an array of sources
+    yandex: true, // Create Yandex browser icon. `boolean` or `{ offset, background }` or an array of sources
   },
-  callback = function (error, response) {
-    if (error) {
-      console.log(error.message) // Error description e.g. "An unknown error has occurred"
-      return
-    }
-    console.log(response.images)   // Array of { name: string, contents: <buffer> }
-    console.log(response.files)    // Array of { name: string, contents: <string> }
-    console.log(response.html)     // Array of strings (html elements)
+  // shortcuts: [
+  //   // Your applications's Shortcuts (see: https://developer.mozilla.org/docs/Web/Manifest/shortcuts)
+  //   // Array of shortcut objects:
+  //   {
+  //     name: "View your Inbox", // The name of the shortcut. `string`
+  //     short_name: "inbox", // optionally, falls back to name. `string`
+  //     description: "View your inbox messages", // optionally, not used in any implemention yet. `string`
+  //     url: "/inbox", // The URL this shortcut should lead to. `string`
+  //     icon: "test/inbox_shortcut.png", // source image(s) for that shortcut. `string`, `buffer` or array of `string`
+  //   },
+  //   // more shortcuts objects
+  // ],
+};
+
+async function doFavicons() {
+  try {
+    const response = await favicons(source, configuration);
+    const savePath = './dist/' + Date.now()
+    await makeDir(savePath)
+    response.images.forEach(item => {
+      if (item.name.includes('favicon-')) {
+        fs.writeFile(savePath + '/' + item.name, item.contents).then(() => {
+          if (item.name.includes('48')) {
+            toIco(item.contents).then(buf => {
+              console.log('sss', buf)
+              fs.writeFile(savePath + '/favicon.ico', buf)
+            })
+          }
+        })
+      }
+    })
+    // console.log(response.images); // Array of { name: string, contents: <buffer> }
+    // console.log(response.files); // Array of { name: string, contents: <string> }
+    // console.log(response.html); // Array of strings (html elements)
+  } catch (error) {
+    console.log(error.message); // Error description e.g. "An unknown error has occurred"
   }
+}
 
-favicons(source, configuration, callback)
-
-
+doFavicons()
